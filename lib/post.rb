4aa7c893e98a8c1ac3c942555d5671ca432bcba6
ayo3able <mrsbarclay101008@gmail.com>
2020-11-08 11:56:43 -0500

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,29 @@
+require 'pry'
+class Post
+
+    attr_accessor :author, :title, :name
+  
+    @@all = []
+  
+    def initialize(title)
+      @title = title
+      save
+    end
+  
+    def save
+      @@all << self
+    end
+  
+    def self.all
+      @@all
+    end
+
+    def author_name
+        #binding.pry
+       if self.author
+        self.author.name
+       else
+        nil
+       end
+    end
+end
